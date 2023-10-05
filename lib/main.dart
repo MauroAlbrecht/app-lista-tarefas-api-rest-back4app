db772e4e02b93faec927a9fbe041cadb962fc8a2
Mestre Dos Lances <appmestredoslances@gmail.com>
2023-10-05 11:25:23 -0300

    add dotenv e interceptor

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:app_tarefas_back4app/my_app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: ".env");
   runApp(MyApp());
 }
--- lib/repositories/tarefas_repository.dart
+++ lib/repositories/tarefas_repository.dart
@@ -1,37 +1,34 @@
 import 'package:app_tarefas_back4app/models/tarefa_model.dart';
 import 'package:dio/dio.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+import 'back4app_custom_dio.dart';
 
 class TarefasRepository {
-  var _dio = Dio();
 
-  TarefasRepository() {
-    _dio.options.headers['X-Parse-Application-Id'] = 'u1G39rCu2Yz4oxj23XdIbQ1Xl6ZuhPOfyY80zhwz';
-    _dio.options.headers['X-Parse-REST-API-Key'] = 'kjA8zFWMVj7gvycpDVE52tjDxvMH8zNMM7Gqr3z0';
-    _dio.options.headers['Content-Type'] = 'application/json';
-    _dio.options.baseUrl = 'https://parseapi.back4app.com/classes';
-  }
+  final _dioCustom = Back4appCustomDio();
 
   Future<TarefasModel> getAll(bool naoConcluidas) async {
     var url = '/Tarefas';
     if (naoConcluidas) {
       url = '$url?where={\"concluido":false}';
     }
-    var result = await _dio.get(url);
+    var result = await _dioCustom.dio.get(url);
     return TarefasModel.fromJson(result.data);
   }
 
   Future<void> criar(TarefaModel tarefaModel) async {
     var url = '/Tarefas';
-    await _dio.post(url, data: tarefaModel.toCreateJson());
+    await _dioCustom.dio.post(url, data: tarefaModel.toCreateJson());
   }
 
   Future<void> atualizar(TarefaModel tarefaModel) async {
     var url = '/Tarefas/${tarefaModel.objectId}';
-    await _dio.put(url, data: tarefaModel.toCreateJson());
+    await _dioCustom.dio.put(url, data: tarefaModel.toCreateJson());
   }
 
   Future<void> remover(TarefaModel tarefaModel) async {
     var url = '/Tarefas/${tarefaModel.objectId}';
-    await _dio.delete(url);
+    await _dioCustom.dio.delete(url);
   }
 }
